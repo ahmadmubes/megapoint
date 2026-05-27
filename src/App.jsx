b70d1a8a12dac94991360ad44042be14ecec1cba
ahmadmubes <45416927+ahmadmubes@users.noreply.github.com>
2026-05-27 22:27:11 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+export default function App(){
+
+return(
+
+<div style={{
+padding:'20px',
+background:'#111827',
+minHeight:'100vh',
+color:'white'
+}}>
+
+<h1>🚀 MegaPoint</h1>
+
+<p>Aplikasi reward Telegram</p>
+
+<button>
+🎬 Tonton Iklan +50
+</button>
+
+</div>
+
+)
+
+}
